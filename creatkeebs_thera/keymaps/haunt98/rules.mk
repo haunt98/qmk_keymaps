7aabe97272cd8d9cd5eee0638bfb387366a2dbf1
Hau Nguyen <hauvipapro@gmail.com>
2023-09-06 20:13:42 +0700

    feat: support thera (1/?)

---
+++ creatkeebs_thera/keymaps/haunt98/rules.mk
@@ -0,0 +1,14 @@
+# See dztech_dz60rgb_wkl/keymaps/haunt98/config.h
+
+VIA_ENABLE = no
+
+LTO_ENABLE = yes
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = yes
+
+MAGIC_ENABLE = no
+
+BOOTMAGIC_ENABLE = yes
