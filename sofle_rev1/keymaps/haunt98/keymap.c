96f04c9acb9f234c369803aa8775b5a14ac84d19
Hau Nguyen <hauvipapro@gmail.com>
2023-10-12 00:39:08 +0700

    feat(qmk): place holder sofle (2/?)

---
+++ sofle_rev1/keymaps/haunt98/keymap.c
@@ -0,0 +1,5 @@
+// Copyright 2023 haunt98 <hauvipapro@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#include QMK_KEYBOARD_H
+#include "haunt98.h"
---
+++ sofle_rev1/keymaps/haunt98/rules.mk
@@ -0,0 +1,5 @@
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_oled_driver.md
+OLED_ENABLE = yes
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_encoders.md
+ENCODER_ENABLE = yes
