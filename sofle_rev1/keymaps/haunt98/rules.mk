206f715e986a446cab1e7d81f393a935d2a6f394
Hau Nguyen <hauvipapro@gmail.com>
2023-10-13 01:24:09 +0700

    feat(qmk): oled, encoder already enabled upstream for sofle

--- sofle_rev1/keymaps/haunt98/rules.mk
+++ sofle_rev1/keymaps/haunt98/rules.mk
@@ -1,8 +1,5 @@
-# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_oled_driver.md
-OLED_ENABLE = yes
-
-# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_encoders.md
-ENCODER_ENABLE = yes
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_debounce_type.md
+DEBOUNCE_TYPE = sym_eager_pk
 
 # https://github.com/qmk/qmk_firmware/blob/master/docs/feature_converters.md
 CONVERT_TO=promicro_rp2040
