612d6b59aa2caa9170a2870e83d2289fb7c69ea7
Hau Nguyen <hauvipapro@gmail.com>
2023-10-13 00:51:34 +0700

    chore(qmk): rename sofle layer

--- sofle_rev1/keymaps/haunt98/keymap.c
+++ sofle_rev1/keymaps/haunt98/keymap.c
@@ -6,9 +6,9 @@
 
 enum layers {
     LAYER_QWERTY,
-    LAYER_LOWER,
-    LAYER_RAISE,
-    LAYER_ADJUST,
+    LAYER_SYM,
+    LAYER_NAV,
+    LAYER_ADJ,
 };
 
 // Inspire from https://github.com/qmk/qmk_firmware/blob/master/keyboards/sofle/keymaps/default/keymap.c
@@ -23,23 +23,23 @@ QK_GESC,        KC_1,     KC_2,     KC_3,             KC_4,    KC_5,    KC_6,   
 KC_TAB,         KC_Q,     KC_W,     KC_E,             KC_R,    KC_T,    KC_Y,             KC_U,     KC_I,     KC_O,    KC_P,     KC_BSPC,
 CTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,             KC_F,    KC_G,    KC_H,             KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
 SC_LSPO,        KC_Z,     KC_X,     KC_C,             KC_V,    KC_B,    KC_MUTE,          XXXXXXX,  KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  SC_RSPC,
-KC_LCTL,        KC_LALT,  KC_LCMD,  MO(LAYER_LOWER),  KC_SPC,  KC_ENT,  MO(LAYER_RAISE),  KC_RCMD,  KC_RALT,  KC_RCTL
+KC_LCTL,        KC_LALT,  KC_LCMD,  MO(LAYER_SYM),  KC_SPC,  KC_ENT,  MO(LAYER_NAV),  KC_RCMD,  KC_RALT,  KC_RCTL
     ),
-    [LAYER_LOWER] = LAYOUT(
+    [LAYER_SYM] = LAYOUT(
 KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
 XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_F12,   KC_BSLS,
 XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
 _______,  KC_LBRC,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_RBRC,  _______,
 XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX
     ),
-    [LAYER_RAISE] = LAYOUT(
+    [LAYER_NAV] = LAYOUT(
 XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
 XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_UP,    XXXXXXX,  XXXXXXX,  XXXXXXX,
 KC_CAPS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_RGHT,  XXXXXXX,  XXXXXXX,
 _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,
 XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX
     ),
-    [LAYER_ADJUST] = LAYOUT(
+    [LAYER_ADJ] = LAYOUT(
 TD(TD_SAFE_BOOT),  EE_CLR,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
 XXXXXXX,           XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
 XXXXXXX,           XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
@@ -51,7 +51,7 @@ XXXXXXX,           XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  _______,  X
 
 // https://github.com/qmk/qmk_firmware/blob/master/docs/ref_functions.md
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, LAYER_LOWER, LAYER_RAISE, LAYER_ADJUST);
+    return update_tri_layer_state(state, LAYER_SYM, LAYER_NAV, LAYER_ADJ);
 }
 
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_oled_driver.md
