2c4ccb640e78635ed9aec7883952b523e553541a
Hau Nguyen <hauvipapro@gmail.com>
2023-10-19 01:02:34 +0700

    feat(qmk): use define to reduce dupe code

--- dztech_dz60rgb_wkl/keymaps/haunt98/keymap.c
+++ dztech_dz60rgb_wkl/keymaps/haunt98/keymap.c
@@ -11,19 +11,21 @@ enum layers {
     LAYER_ADJ,
 };
 
+#define MO_NAV MO(LAYER_NAV)
+#define MO_OPT MO(LAYER_OPT)
+
 // Inspire from https://github.com/qmk/qmk_firmware/blob/master/keyboards/dztech/dz60rgb_wkl/keymaps/hhkb/keymap.c
 // https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes.md
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_grave_esc.md
-// https://github.com/qmk/qmk_firmware/blob/master/docs/mod_tap.md
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_space_cadet.md
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_QWERTY] = LAYOUT_60_tsangan_hhkb(
-        QK_GESC,        KC_1,     KC_2,     KC_3,    KC_4,     KC_5,           KC_6,    KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,         KC_BSLS,  KC_GRV,
-        KC_TAB,         KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,           KC_Y,    KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,        KC_BSPC,
-        CTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,           KC_H,    KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,
-        SC_LSPO,        KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,           KC_N,    KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  SC_RSPC,  MO(LAYER_NAV),
-        XXXXXXX,        KC_LOPT,  KC_LCMD,  KC_SPC,  KC_RCMD,  MO(LAYER_OPT),  XXXXXXX
+        QK_GESC,  KC_1,     KC_2,     KC_3,    KC_4,     KC_5,    KC_6,    KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
+        CTL_ESC,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,    KC_H,    KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,
+        SC_LSPO,  KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  SC_RSPC,  MO_NAV,
+        XXXXXXX,  KC_LOPT,  KC_LCMD,  KC_SPC,  KC_RCMD,  MO_OPT,  XXXXXXX
     ),
     [LAYER_NAV] = LAYOUT_60_tsangan_hhkb(
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   XXXXXXX,  XXXXXXX,
