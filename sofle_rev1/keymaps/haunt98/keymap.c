1b2556481807dafa46d39666394967bb4b53694f
Hau Nguyen <hauvipapro@gmail.com>
2023-10-23 15:46:27 +0700

    feat(qmk): revision sofle_rev1

--- sofle_rev1/keymaps/haunt98/keymap.c
+++ sofle_rev1/keymaps/haunt98/keymap.c
@@ -16,20 +16,19 @@ enum layers {
 
 // Inspire from https://github.com/qmk/qmk_firmware/blob/master/keyboards/sofle/keymaps/default/keymap.c
 // https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes.md
-// https://github.com/qmk/qmk_firmware/blob/master/docs/feature_grave_esc.md
 // https://github.com/qmk/qmk_firmware/blob/master/docs/mod_tap.md
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_space_cadet.md
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_QWERTY] = LAYOUT(
-        QK_GESC,   KC_1,     KC_2,        KC_3,        KC_4,    KC_5,    KC_6,        KC_7,     KC_8,     KC_9,     KC_0,     KC_GRV,
+        KC_GRV,    KC_1,     KC_2,        KC_3,        KC_4,    KC_5,    KC_6,        KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
         KC_TAB,    KC_Q,     KC_W,        KC_E,        KC_R,    KC_T,    KC_Y,        KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
         CTL_ESC,   KC_A,     KC_S,        KC_D,        KC_F,    KC_G,    KC_H,        KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_ENT,
         SC_LSPO,   KC_Z,     KC_X,        KC_C,        KC_V,    KC_B,    KC_MUTE,     KC_CAPS,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  SC_RSPC,
         LOPT_BRC,  KC_LCMD,  LT_SYM_SPC,  LT_SYM_SPC,  KC_SPC,  KC_ENT,  LT_NAV_SPC,  KC_SPC,   KC_RCMD,  ROPT_BRC
     ),
     [LAYER_SYM] = LAYOUT(
-        KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_COLN,  KC_MINS,  KC_UNDS,  KC_EQL,   KC_PLUS,  XXXXXXX,  KC_F12,
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LBRC,  KC_LCBR,  KC_RCBR,  KC_RBRC,  XXXXXXX,  _______,
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  KC_BSLS,  KC_PIPE,  KC_QUOT,  KC_DQUO,  XXXXXXX,  _______,
