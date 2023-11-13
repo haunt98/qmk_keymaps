f14c2e62a44cea7dee7eb27f2c32c8d1e2559fb0
Hau Nguyen <hauvipapro@gmail.com>
2023-11-13 14:20:15 +0700

    feat(qmk): revision sofle_rev1

--- sofle_rev1/keymaps/haunt98/keymap.c
+++ sofle_rev1/keymaps/haunt98/keymap.c
@@ -11,7 +11,7 @@ enum layers {
     LAYER_ADJ,
 };
 
-#define LT_SYM_SPC LT(LAYER_SYM, KC_SPC)
+#define MO_SYM MO(LAYER_SYM)
 #define MO_NAV MO(LAYER_NAV)
 
 // Inspire from https://github.com/qmk/qmk_firmware/blob/master/keyboards/sofle/keymaps/default/keymap.c
@@ -21,11 +21,11 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_QWERTY] = LAYOUT(
-        KC_GRV,    KC_1,     KC_2,        KC_3,        KC_4,    KC_5,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-        KC_TAB,    KC_Q,     KC_W,        KC_E,        KC_R,    KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-        CTL_ESC,   KC_A,     KC_S,        KC_D,        KC_F,    KC_G,    KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_ENT,
-        SC_LSPO,   KC_Z,     KC_X,        KC_C,        KC_V,    KC_B,    KC_MUTE,  KC_CAPS,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  SC_RSPC,
-        LOPT_BRC,  KC_LCMD,  LT_SYM_SPC,  LT_SYM_SPC,  KC_SPC,  KC_ENT,  MO_NAV,   KC_SPC,   KC_RCMD,  ROPT_BRC
+        KC_GRV,    KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+        KC_TAB,    KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+        CTL_ESC,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_ENT,
+        SC_LSPO,   KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,  KC_CAPS,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  SC_RSPC,
+        LOPT_BRC,  KC_LCMD,  KC_SPC,  MO_SYM,  KC_SPC,  KC_ENT,  MO_NAV,   KC_SPC,   KC_RCMD,  ROPT_BRC
     ),
     [LAYER_SYM] = LAYOUT(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
