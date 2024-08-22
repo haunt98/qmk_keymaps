3f4be6db6a73ce55e522ca3a221224359218a879
Hau Nguyen <hauvipapro@gmail.com>
2024-08-22 23:18:38 +0700

    wip

--- hineybush_h60/keymaps/haunt98/config.h
+++ hineybush_h60/keymaps/haunt98/config.h
@@ -15,6 +15,22 @@
 #undef QUICK_TAP_TERM
 #define QUICK_TAP_TERM 120
 
+// https://github.com/qmk/qmk_firmware/blob/master/docs/features/rgblight.md
+// https://www.youtube.com/watch?v=VKrpPAHlisY
+#ifdef RGBLIGHT_ENABLE
+#    undef RGBLIGHT_ANIMATIONS
+// #undef RGBLIGHT_EFFECT_ALTERNATING
+// #undef RGBLIGHT_EFFECT_BREATHING
+// #undef RGBLIGHT_EFFECT_CHRISTMAS
+// #undef RGBLIGHT_EFFECT_KNIGHT
+// #undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    undef RGBLIGHT_EFFECT_RGB_TEST
+// #undef RGBLIGHT_EFFECT_SNAKE
+// #undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #undef RGBLIGHT_EFFECT_TWINKLE
+#endif
+
 // https://github.com/qmk/qmk_firmware/blob/master/docs/config_options.md
 // https://github.com/qmk/qmk_firmware/issues/17316
 #define USB_SUSPEND_WAKEUP_DELAY 200
