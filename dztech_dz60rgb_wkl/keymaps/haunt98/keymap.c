c46050ee24d403f2ba883f5c6a10d12ecb46afd8
Hau Nguyen <hauvipapro@gmail.com>
2024-09-04 00:43:23 +0700

    feat(qmk): LAYOUT_60_tsangan_hhkb -> LAYOUT_60_ansi_tsangan_split_bs_rshift
    See https://github.com/qmk/qmk_firmware/pull/24186

--- dztech_dz60rgb_wkl/keymaps/haunt98/keymap.c
+++ dztech_dz60rgb_wkl/keymaps/haunt98/keymap.c
@@ -10,28 +10,28 @@
 // https://github.com/qmk/qmk_firmware/blob/master/docs/features/space_cadet.md
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [LAYER_QWERTY] = LAYOUT_60_tsangan_hhkb(
+    [LAYER_QWERTY] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
         QK_GESC,  KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,    KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,
         KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,    KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
         CTL_ESC,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,    KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,
         SC_LSPO,  KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,     KC_N,    KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  SC_RSPC,  TL_LOWR,
         XXXXXXX,  KC_LOPT,  KC_LCMD,  KC_SPC,  KC_RCMD,  TL_UPPR,  XXXXXXX
     ),
-    [LAYER_LOWER] = LAYOUT_60_tsangan_hhkb(
+    [LAYER_LOWER] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   XXXXXXX,  XXXXXXX,
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_UP,    XXXXXXX,  KC_DEL,
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_RGHT,  XXXXXXX,
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_DOWN,  _______,  XXXXXXX,
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX
     ),
-    [LAYER_UPPER] = LAYOUT_60_tsangan_hhkb(
+    [LAYER_UPPER] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         XXXXXXX,  KC_BRMD,  KC_BRMU,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         KC_CAPS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         _______,  RGB_HUI,  RGB_SAI,  RGB_VAI,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,
         XXXXXXX,  RGB_TOG,  RGB_MOD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
     ),
-    [LAYER_ADJUST] = LAYOUT_60_tsangan_hhkb(
+    [LAYER_ADJUST] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
         QK_BOOT,  EE_CLR,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         XXXXXXX,           XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         XXXXXXX,           XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
