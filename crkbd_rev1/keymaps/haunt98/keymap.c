5cbc3b40ca40c694cbb443fa19ec152925651fc3
Hau Nguyen <hauvipapro@gmail.com>
2025-08-15 00:58:30 +0700

    feat: update keymaps for sofle_rev1, crkbd_rev1

--- crkbd_rev1/keymaps/haunt98/keymap.c
+++ crkbd_rev1/keymaps/haunt98/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [LAYER_LOWER] = LAYOUT_split_3x6_3(
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LBRC,  KC_RBRC,  KC_7,  KC_8,  KC_9,  KC_0,     XXXXXXX,  _______,
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_4,  KC_5,  KC_6,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_4,  KC_5,  KC_6,  XXXXXXX,  XXXXXXX,  _______,
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_1,  KC_2,  KC_3,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______
     ),
