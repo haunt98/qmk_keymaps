16fbbc38468ff48ceb72c2d41fd649c010d36c59
Hau Nguyen <hauvipapro@gmail.com>
2025-11-26 21:59:23 +0700

    feat(qmk): put - right next to 0

--- crkbd_rev1/keymaps/haunt98/keymap.c
+++ crkbd_rev1/keymaps/haunt98/keymap.c
@@ -16,13 +16,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCMD,  TL_LOWR,  KC_SPC,  KC_SPC,  TL_UPPR,  KC_ROPT
     ),
     [LAYER_LOWER] = LAYOUT_split_3x6_3(
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LBRC,  KC_RBRC,  KC_7,  KC_8,  KC_9,  KC_0,     XXXXXXX,  _______,
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LBRC,  KC_RBRC,  KC_7,  KC_8,  KC_9,  KC_0,     KC_MINS,  _______,
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_4,  KC_5,  KC_6,  XXXXXXX,  XXXXXXX,  _______,
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_1,  KC_2,  KC_3,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______
     ),
     [LAYER_UPPER] = LAYOUT_split_3x6_3(
-        _______,  _______,  _______,  KC_F7,    KC_F8,    KC_F9,   KC_LCBR,  KC_RCBR,  KC_EQL,   KC_PLUS,  XXXXXXX,  _______,
+        _______,  _______,  _______,  KC_F7,    KC_F8,    KC_F9,   KC_LCBR,  KC_RCBR,  KC_EQL,   KC_PLUS,  KC_MINS,  _______,
         _______,  XXXXXXX,  XXXXXXX,  KC_F4,    KC_F5,    KC_F6,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_COLN,  _______,
         _______,  XXXXXXX,  XXXXXXX,  KC_F1,    KC_F2,    KC_F3,   KC_BSLS,  KC_PIPE,  KC_QUOT,  KC_DQUO,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______
