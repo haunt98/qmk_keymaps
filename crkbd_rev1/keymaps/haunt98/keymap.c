e8d94330573f19b8c9b08282dbacedbeb0bcfe4b
Hau Nguyen <hauvipapro@gmail.com>
2026-05-12 16:52:50 +0700

    feat: use home row mods for s/alt

--- crkbd_rev1/keymaps/haunt98/keymap.c
+++ crkbd_rev1/keymaps/haunt98/keymap.c
@@ -10,10 +10,10 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_QWERTY] = LAYOUT_split_3x6_3(
-        KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_BSPC,
-        CTL_ESC,  KC_A,     KC_S,    KC_D,    KC_F,     KC_G,    KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_ENT,
-        SC_LSPO,  KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  SC_RSPC,
-        KC_LCMD,  TL_LOWR,  KC_SPC,  KC_SPC,  TL_UPPR,  KC_ROPT
+        KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,    KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_BSPC,
+        CTL_ESC,  KC_A,     OPT_KCS,  KC_D,    KC_F,     KC_G,    KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_ENT,
+        SC_LSPO,  KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  SC_RSPC,
+        KC_LCMD,  TL_LOWR,  KC_SPC,   KC_SPC,  TL_UPPR,  KC_ROPT
     ),
     [LAYER_LOWER] = LAYOUT_split_3x6_3(
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LBRC,  KC_RBRC,  KC_7,  KC_8,  KC_9,  KC_0,     KC_MINS,  _______,
--- users/haunt98/haunt98.h
+++ users/haunt98/haunt98.h
@@ -9,6 +9,7 @@
 
 // https://github.com/qmk/qmk_firmware/blob/master/docs/mod_tap.md
 #define CTL_ESC CTL_T(KC_ESC)
+#define OPT_KCS OPT_T(KC_S)
 
 // Use only 4 layers
 enum layers {
